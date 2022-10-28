3fe23da937eff9430da2dbe7b10ae2e12f470f6b
Dhruvanshu1775 <Dhruvanshu1775@gmail.com>
2022-10-28 11:50:52 +0530

    save

--- Invoice/settings.py
+++ Invoice/settings.py
@@ -134,6 +134,8 @@ if DEBUG:
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
